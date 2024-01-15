fe78ecacb7b8fa50fe8d2ba6428958783a4ee420
Kihun Kim <kimkihun811@gmail.com>
2024-01-15 13:51:25 +0900

    16진수

--- Day 02/py2.py
+++ Day 02/py2.py
@@ -22,10 +22,21 @@
 # print('밑은 %d, 지수는 %d, 결과 값은 %d' %(base_number, exponent_number, base_number**exponent_number))
 
 
-first_number = int(input("First number : "))
-second_number = int(input("Second number : "))
-quotient = first_number // second_number
-remainder = first_number % second_number
-print(f'몫은 {quotient}, 나머지는 {remainder} 입니다.')
-print(f'몫은 {divmod(first_number, second_number)[0]}, 나머지는 {divmod(first_number,second_number)[1]} 입니다.')
+# first_number = int(input("First number : "))
+# second_number = int(input("Second number : "))
+# quotient = first_number // second_number
+# remainder = first_number % second_number
+# print(f'몫은 {quotient}, 나머지는 {remainder} 입니다.')
+# print(f'몫은 {divmod(first_number, second_number)[0]}, 나머지는 {divmod(first_number,second_number)[1]} 입니다.')
 # print(divmod(9,5)) 위처럼 몫과 나머지를 한번에 나타내주는 함수
+
+# print(-5**2) # 25가 아니라 -25로 출력
+
+#10진수, 8진수, 16진수, 2진수
+# dec = 65
+# octal = 0o101
+# hexadecimal = 0x41
+# binary = 0b01000001
+# print(dec, octal, hexadecimal, binary)
+# print(chr(binary)) #ascii코드
+# print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
