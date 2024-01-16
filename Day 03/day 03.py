6feaa6f6f268ce994f5d8a561a25befa64b30796
Kihun Kim <kimkihun811@gmail.com>
2024-01-16 10:17:47 +0900

    역방향 인덱싱

--- Day 03/day 03.py
+++ Day 03/day 03.py
@@ -6,7 +6,11 @@
 # print(university) #줄바꿈이 아닌 쓴 그대로 보여줌
 
 #Duplicate with *
-start = 'Na' *4 + '\n'
-middle = 'Hey' *3 + '\n'
-end = 'Goodbye.'
-print(start + start + middle + end) #문자열과 *는 함께 사용이 가능하다.
+# start = 'Na' *4 + '\n'
+# middle = 'Hey' *3 + '\n'
+# end = 'Goodbye.'
+# print(start + start + middle + end) #문자열과 *는 함께 사용이 가능하다.
+
+#역방향 인덱싱
+letters = 'abcdefghijklmnopqrstuvwxyz'
+print(letters[-1])
