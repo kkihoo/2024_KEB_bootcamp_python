075654200c36c90c6c479b665845eb7dfeacb6db
Kihun Kim <kimkihun811@gmail.com>
2024-01-16 10:16:07 +0900

    문자열과 *

--- Day 03/day 03.py
+++ Day 03/day 03.py
@@ -5,3 +5,8 @@
 # university = r"Inha\nUniversity!"
 # print(university) #줄바꿈이 아닌 쓴 그대로 보여줌
 
+#Duplicate with *
+start = 'Na' *4 + '\n'
+middle = 'Hey' *3 + '\n'
+end = 'Goodbye.'
+print(start + start + middle + end) #문자열과 *는 함께 사용이 가능하다.
