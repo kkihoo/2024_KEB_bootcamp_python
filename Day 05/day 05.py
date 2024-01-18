4707a4b55f5e0ab9f3d15718c3c65e54b6f0df28
Kihun Kim <kimkihun811@gmail.com>
2024-01-18 10:01:00 +0900

    set 연산자

--- Day 05/day 05.py
+++ Day 05/day 05.py
@@ -4,8 +4,20 @@
 # count_alphabet = {letter: player.count(letter) for letter in player}
 # print(count_alphabet)
 
-player = 'Minjae Kim'
-count_alphabet = dict()
-for letter in player :
-    count_alphabet[letter] = player.count(letter)
-print(count_alphabet)
+# player = 'Minjae Kim'
+# count_alphabet = dict()
+# for letter in player :
+#     count_alphabet[letter] = player.count(letter)
+# print(count_alphabet)
+
+#set
+#연산자
+a = {1, 2}
+b = {2, 3}
+print(a & b) # 교집합 or a.intersection(b)
+print(a | b) # 합집합 or a.union(b)
+print(a - b) # 차집합 or a.difference(b)
+print(a ^ b) # 대칭 차집합 or a.symmetric_difference(b)
+print(a <= b) # 부분집합 or a.issubset(b)
+print(a >= b) # 상위집합 or a.issuperset(b)
+
