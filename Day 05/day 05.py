c7c4d02f3ea55f1df7826caaf398ed2bf04cce30
Kihun Kim <kimkihun811@gmail.com>
2024-01-18 10:01:19 +0900

    set frozenset()

--- Day 05/day 05.py
+++ Day 05/day 05.py
@@ -12,12 +12,16 @@
 
 #set
 #연산자
-a = {1, 2}
-b = {2, 3}
-print(a & b) # 교집합 or a.intersection(b)
-print(a | b) # 합집합 or a.union(b)
-print(a - b) # 차집합 or a.difference(b)
-print(a ^ b) # 대칭 차집합 or a.symmetric_difference(b)
-print(a <= b) # 부분집합 or a.issubset(b)
-print(a >= b) # 상위집합 or a.issuperset(b)
+# a = {1, 2}
+# b = {2, 3}
+# print(a & b) # 교집합 or a.intersection(b)
+# print(a | b) # 합집합 or a.union(b)
+# print(a - b) # 차집합 or a.difference(b)
+# print(a ^ b) # 대칭 차집합 or a.symmetric_difference(b)
+# print(a <= b) # 부분집합 or a.issubset(b)
+# print(a >= b) # 상위집합 or a.issuperset(b)
 
+# frozenset() 수정이 불가능한 객체
+fs = frozenset([5,1,2])
+print(fs)
+fs.add(4) # 추가 안됨 에러 뜸
