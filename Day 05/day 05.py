f1bc6ecb4126aae6ae19ee8e4ef703af04129ca1
Kihun Kim <kimkihun811@gmail.com>
2024-01-18 15:47:10 +0900

    function v5 decorator

--- Day 05/day 05.py
+++ Day 05/day 05.py
@@ -62,14 +62,44 @@
 # z = lambda x :pow(x,2)
 # print(tuple(map(z, even_numbers)))
 
-def my_range(first=0, last=5, step=1):
-    number = first
-    while number < last :
-        yield number # 제너레이터는 return 대신 yield
-        number += step
+# def my_range(first=0, last=5, step=1):
+#     number = first
+#     while number < last :
+#         yield number # 제너레이터는 return 대신 yield
+#         number += step
+#
+# r = my_range()
+# print(r, type(r))
+#
+# for x in r:
+#     print(x)
 
-r = my_range()
-print(r, type(r))
+##decorators
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args)
+        return r
+    return inner
 
-for x in r:
-    print(x)
+@description
+def squares(n):
+    """
+    제곱 함수
+    """
+    return n * n
+@description
+def power(b, e):
+    """
+    거듭제곱 함수
+    """
+    result = 1
+    for _ in range(e):
+        result = result * b
+    return result
+
+f1 = description(squares)
+print(f1(9))
+print(power(2, 10))
+print(squares(4))
