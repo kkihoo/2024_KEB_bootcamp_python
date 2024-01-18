6af764a1151510c39e9710160a0e75ddcfd54bd0
Kihun Kim <kimkihun811@gmail.com>
2024-01-18 13:36:49 +0900

    function v1

--- Day 05/day 05.py
+++ Day 05/day 05.py
@@ -22,6 +22,22 @@
 # print(a >= b) # 상위집합 or a.issuperset(b)
 
 # frozenset() 수정이 불가능한 객체
-fs = frozenset([5,1,2])
-print(fs)
-fs.add(4) # 추가 안됨 에러 뜸
+# fs = frozenset([5,1,2])
+# print(fs)
+# fs.add(4) # 추가 안됨 에러 뜸
+
+## 함수
+def squares(*n)->list:
+    """
+    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
+    :param n: tuple
+    :return: list
+    """
+    return [pow(i, 2) for i in n]
+    # return n*n
+
+def run_function(f, *number):
+    return f(*number)
+
+print(squares(7,5,2,6,3))
+print(run_function(squares, 9,10))
