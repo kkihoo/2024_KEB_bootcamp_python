e5f866a1bb27170a2a19b0d1b8e88f223d6d27db
Kihun Kim <kimkihun811@gmail.com>
2024-01-19 10:20:21 +0900

    재귀함수(factorial)

---
+++ Day 06/day 06.py
@@ -0,0 +1,16 @@
+# 재귀 함수
+
+def factorial_repetition(n) -> int:
+    """
+    반복문을 이용한 팩토리얼 함수
+    :param n:
+    """
+    result = 1
+    for i in range(2, 2+1):
+        result = result * i
+
+
+def factorial_recursion(n):
+    '''
+
+    '''
